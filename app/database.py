5473df869f51fb1972bdc52b360a43bb33be84dd
Leonardo Zani de Souza <leonardozanidev@gmail.com>
2026-09-12 09:36:12 -0300

    feat: finalizado configuração de conexão com o banco

---
+++ app/database.py
@@ -0,0 +1,22 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
+from dotenv import load_dotenv
+import os
+
+# Carregando as variáveis de ambiente para dentro do arquivo
+load_dotenv()
+
+# Importando variável para montar a string de conexão com o banco de dados
+db_user = os.getenv("DB_USER")
+db_pass = os.getenv("DB_PASS")
+db_host = os.getenv("DB_HOST")
+db_port = os.getenv("DB_PORT")
+db_name = os.getenv("DB_NAME")
+conn_string = f"postgresql://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}"
+
+engine = create_engine(conn_string)
+SessionLocal = sessionmaker(bind=engine)
+
+# Se chamar DeclarativeBase() vai estar tentando instanciar DeclarativeBase, se chamar DeclarativeBase vai herdar
+class Base(DeclarativeBase):
+    pass
--- app/main.py
+++ app/main.py
@@ -1,10 +1,15 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+app_name = os.getenv("APP_NAME")
 
 app = FastAPI()
 
 @app.get("/")
 def get_root():
-    return {"msg":"Bem-Vindo à minha API."}
+    return {"msg":f"Bem-Vindo à minha {app_name}."}
 
 @app.get("/health")
 def get_health():
